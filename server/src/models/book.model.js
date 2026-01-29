008d38175cccef3d8310d07f0b714495cc5dd96f
Prakash Kumar <prakashyadav9576@gmail.com>
2026-01-29 16:40:32 +0530

    make title unique

--- server/src/models/book.model.js
+++ server/src/models/book.model.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     description: {
         type: String,
