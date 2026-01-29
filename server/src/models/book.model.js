e415de82ae8473165427861d26abaa63ec7bd0f1
Prakash Kumar <prakashyadav9576@gmail.com>
2026-01-29 16:08:34 +0530

    create book schema

---
+++ server/src/models/book.model.js
@@ -0,0 +1,34 @@
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    author: {
+        type: String
+    },
+    department: {
+        type: String,
+        required: true,
+        enum: ["CSE", "IT", "ECE", "EEE", "MECH", "CIVIL", "MBA", "MCA", "BBA", "BCA", "B.COM", "B.SC", "B.PHARM", "B.ARCH", "B.DES", "B.ED", "B.LLB", "B.PT", "B.HM", "B.MS", "B.AS", "B.FA", "B.FT"]
+    },
+    isbn: {
+        type: String,
+        unique: true
+    },
+    publisher: {
+        type: String,
+    },
+    edition: {
+        type: String,
+    },
+    cover_url: {
+        type: String,
+    },
+    copies: {
+        type: [String],
+    }
+
+});
