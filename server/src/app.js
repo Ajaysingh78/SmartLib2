34591fbc7786e41be27374d82a764640f880f2fc
Prakash Kumar <prakashyadav9576@gmail.com>
2026-01-29 15:48:17 +0530

    init

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+    res.send("API running 🚀");
+});
+
+export default app;
