14fa2c2a9b7c6afde7d2535d0e41d5d996e65775
Prakash Kumar <prakashyadav9576@gmail.com>
2026-02-01 12:35:26 +0530

    middleware to check is admin logedin or not

---
+++ server/src/middlewares/checkAdminLogedIn.js
@@ -0,0 +1,15 @@
+function isAdminLogedIn(req, res, next) {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({ status: "failed", message: "Unauthorized" });
+        }
+        const decodedToken = verifyToken(token);
+        req.adminId = decodedToken.id;
+        next();
+    } catch (error) {
+        res.status(500).json({ status: "failed", message: "Internal server error", error });
+    }
+}
+
+export default isAdminLogedIn;
